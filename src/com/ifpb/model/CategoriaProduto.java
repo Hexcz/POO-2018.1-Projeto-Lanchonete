e252037f823f06dc20b8018ceb2824518abbd09b
Lethícia <lethiciacl@hotmail.com>
2018-09-03 23:28:19 -0300

    Javadoc no CategoriaProduto.

--- src/com/ifpb/model/CategoriaProduto.java
+++ src/com/ifpb/model/CategoriaProduto.java
@@ -1,5 +1,13 @@
 package com.ifpb.model;
 
+/**
+ * Este ENUM foi criado para listar mais organizadamente as categorias de lanches do sistema da Lanchonete.
+ * @author Hexcz
+ * @author lethiciacl
+ * @version 1.0
+ * @since 1.0
+ * */
+
 public enum CategoriaProduto {
     LANCHE,
     BEBIDA,
