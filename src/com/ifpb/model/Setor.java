ea0c111ef6936c04fac28cb7bd26849774866533
Lethícia <lethiciacl@hotmail.com>
2018-09-03 23:25:19 -0300

    Javadoc no ENUM.

--- src/com/ifpb/model/Setor.java
+++ src/com/ifpb/model/Setor.java
@@ -1,5 +1,13 @@
 package com.ifpb.model;
 
+/**
+ * Este ENUM foi criado para listar mais organizadamente os setores do sistema da Lanchonete.
+ * @author Hexcz
+ * @author lethiciacl
+ * @version 1.0
+ * @since 1.0
+ * */
+
 public enum Setor {
     ATENDIMENTO,
     COZINHA,
