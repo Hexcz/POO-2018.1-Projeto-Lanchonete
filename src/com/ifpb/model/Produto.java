ad735f798df527e70a970bd69f3bc1b67873d417
Lethícia <lethiciacl@hotmail.com>
2018-09-03 23:16:58 -0300

    Adição do javadoc à classe Produto.

--- src/com/ifpb/model/Produto.java
+++ src/com/ifpb/model/Produto.java
@@ -2,6 +2,14 @@ package com.ifpb.model;
 
 import java.util.Objects;
 
+/**
+ * Esta função tem por objetivo modelar a entidade Produto do sistema da Lanchonete.
+ * @author Hexcz
+ * @author lethiciacl
+ * @version 1.0
+ * @since 1.0
+ * */
+
 public class Produto {
 
     private int codigo;
@@ -10,6 +18,10 @@ public class Produto {
     private String descricao;
     private CategoriaProduto categoria;
 
+    /**
+     * CONSTRUTORES.
+     * */
+
     public Produto(int codigo, double preco, String nome, String descricao, CategoriaProduto categoria) {
         this.codigo = codigo;
         this.preco = preco;
@@ -17,6 +29,10 @@ public class Produto {
         this.descricao = descricao;
         this.categoria = categoria;
     }
+
+    /**
+     * GETTERS AND SETTERS.
+     * */
 
     public int getCodigo() {
         return codigo;
@@ -57,6 +73,10 @@ public class Produto {
     public void setCategoria(CategoriaProduto categoria) {
         this.categoria = categoria;
     }
+
+    /**
+     * MÉTODOS SOBRESCRITOS.
+     * */
 
     @Override
     public boolean equals(Object o) {
