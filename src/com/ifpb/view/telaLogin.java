91af3d02f3d9ebc555ab3e259e23e4c63fb216d2
Lethícia <lethiciacl@hotmail.com>
2018-10-01 18:34:51 -0300

    Implementação da tela de login funcional.

--- src/com/ifpb/view/telaLogin.java
+++ src/com/ifpb/view/telaLogin.java
@@ -1,10 +1,14 @@
 package com.ifpb.view;
 
+import com.ifpb.control.FuncionarioDao;
+import com.ifpb.control.FuncionarioImpDao;
+import com.ifpb.model.Funcionario;
 import jdk.nashorn.internal.scripts.JO;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class telaLogin extends JFrame {
     private JPanel contentPane;
@@ -15,16 +19,50 @@ public class telaLogin extends JFrame {
     private JLabel senha;
     private JPanel panelMain;
     private JButton autenticarButton;
+    private FuncionarioDao userDao;
 
     public telaLogin() {
+
+        try{
+            userDao = new FuncionarioImpDao();
+        }catch(IOException ex){
+            JOptionPane.showMessageDialog(null, "Falha no arquivo.", "Mensagem de Erro", JOptionPane.ERROR_MESSAGE);
+        }
+
         setContentPane(contentPane);
-        getRootPane().setDefaultButton(autenticarButton);
+        setTitle("Tela Login");
+//        getRootPane().setDefaultButton(autenticarButton);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         autenticarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                onAutenticar();
+                String user = username.getText();
+                String passwd = new String(password.getPassword());
+                Funcionario func = null;
+
+                try{
+                    func = userDao.buscarPorUsername(user);
+                }catch(IOException|ClassNotFoundException ex){
+                    JOptionPane.showMessageDialog(null, "Falha no arquivo", "Mensagem de Erro", JOptionPane.ERROR_MESSAGE);
+                }
+
+                if(func == null){
+                    System.out.println("deu merda");
+                }
+
+                if(func!=null){
+                    if(func.verifyLogin(user,passwd)){
+                        telaPrincipal menuOpcoes = new telaPrincipal();
+                        menuOpcoes.pack();
+                        menuOpcoes.setVisible(true);
+                        dispose();
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Senha incorreta.", "Mensagem de Erro",JOptionPane.ERROR_MESSAGE);
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(null, "Usuário não cadastrado.", "Mensagem de Erro",JOptionPane.ERROR_MESSAGE);
+                }
             }
         });
 
@@ -36,10 +74,30 @@ public class telaLogin extends JFrame {
         });
     }
 
-    private void onAutenticar(){
-        JOptionPane.showMessageDialog(null, "Autenticado com sucesso");
-        dispose();
-    }
+//    private void onAutenticar(){
+//        String user = username.getText();
+//        String passwd = new String(password.getPassword());
+//        Funcionario func = null;
+//
+//        try{
+//            func = userDao.buscarPorUsername(user);
+//        }catch(IOException|ClassNotFoundException ex){
+//            JOptionPane.showMessageDialog(null, "Falha no arquivo", "Mensagem de Erro", JOptionPane.ERROR_MESSAGE);
+//        }
+//
+//        if(func!=null){
+//            if(func.verifyLogin(user,passwd)){
+//                telaPrincipal menuOpcoes = new telaPrincipal();
+//                menuOpcoes.pack();
+//                menuOpcoes.setVisible(true);
+//                dispose();
+//            }else{
+//                JOptionPane.showMessageDialog(null, "Senha incorreta.", "Mensagem de Erro",JOptionPane.ERROR_MESSAGE);
+//            }
+//        }else{
+//                JOptionPane.showMessageDialog(null, "Usuário não cadastrado.", "Mensagem de Erro",JOptionPane.ERROR_MESSAGE);
+//            }
+//        }
 
     private void onCriarNovaConta(){
         telaCadastro cadastro = new telaCadastro();
