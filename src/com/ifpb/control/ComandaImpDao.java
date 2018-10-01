2a2cc7e6a8f39cc13c21bb6e20d6d2c314bce96a
Lethícia <lethiciacl@hotmail.com>
2018-10-01 16:29:48 -0300

    Implementação da classe ComandaImpDao.

---
+++ src/com/ifpb/control/ComandaImpDao.java
@@ -0,0 +1,88 @@
+package com.ifpb.control;
+
+import com.ifpb.model.Comanda;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ComandaImpDao implements ComandaDao {
+
+    private File file;
+
+    public ComandaImpDao() throws IOException{
+        file = new File("Comanda");
+
+        if(!file.exists()){
+            file.createNewFile();
+        }
+    }
+
+    private void attArchive(Set<Comanda> comandas) throws IOException {
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))){
+            out.writeObject(file);
+        }
+    }
+
+    @Override
+    public Set<Comanda> getComandas() throws ClassNotFoundException, IOException {
+        if(file.length()>0){
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))){
+            return (Set<Comanda>) in.readObject();
+        }}else{
+            return new HashSet<>();
+        }
+    }
+
+    @Override
+    public boolean salvar(Comanda c) throws ClassNotFoundException, IOException {
+        Set<Comanda> comandas = getComandas();
+        if(comandas.add(c)){
+            attArchive(comandas);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public boolean atualizar(Comanda comanda) throws ClassNotFoundException, IOException {
+        Set<Comanda> comandas = getComandas();
+        Comanda c = new Comanda(comanda.getNumeroComanda());
+        if(comandas.remove(c)){
+            comandas.add(comanda);
+            attArchive(comandas);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deletar(Comanda comanda) throws ClassNotFoundException, IOException {
+        Set<Comanda> comandas = getComandas();
+        Comanda c = new Comanda(comanda.getNumeroComanda());
+        if(comandas.contains(c)){
+            comanda.setComandaAberta(false);
+            comandas.remove(c);
+            comandas.add(comanda);
+            attArchive(comandas);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public Comanda buscarPorNumero(int numero) throws ClassNotFoundException, IOException {
+        Set<Comanda> comandas = getComandas();
+        for(Comanda comanda : comandas){
+            if(comanda.getNumeroComanda() == numero){
+                return comanda;
+            }
+        }
+        return null;
+    }
+
+
+}
