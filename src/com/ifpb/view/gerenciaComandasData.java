72f0137444054cb5e76951abd202d1a922c7c27d
Lethícia <lethiciacl@hotmail.com>
2018-10-04 18:28:44 -0300

    Implementação funcional da janela GerenciaComandaDatas.

--- src/com/ifpb/view/gerenciaComandasData.java
+++ src/com/ifpb/view/gerenciaComandasData.java
@@ -3,26 +3,27 @@ package com.ifpb.view;
 import com.ifpb.control.ComandaDao;
 import com.ifpb.control.ComandaImpDao;
 import com.ifpb.model.Comanda;
+import jdk.nashorn.internal.scripts.JO;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.text.MaskFormatter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashSet;
 import java.util.Set;
 
 public class gerenciaComandasData extends JFrame {
     private JPanel contentPane;
-    private JFormattedTextField formattedTextField1;
-    private JFormattedTextField formattedTextField2;
+    private JFormattedTextField diniciofd;
+    private JFormattedTextField dfimfd;
     private JTable table1;
     private JButton buscarButton;
-    private JButton buttonOK;
+    private JScrollPane scrollPane1;
     private ComandaDao cdao;
     private Set<Comanda> comandasInt;
     private JLabel valorTotal;
@@ -35,74 +36,102 @@ public class gerenciaComandasData extends JFrame {
             cdao = new ComandaImpDao();
         }catch(IOException ex){
             JOptionPane.showMessageDialog(null, ex.getMessage(), "Mensagem de erro", JOptionPane.ERROR_MESSAGE);
-
         }
+
         setContentPane(contentPane);
-        DateTimeFormatter frm = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        setLocationRelativeTo(null);
 
-        try{
-            dataInicio = LocalDate.parse(formattedTextField1.getText(),frm);
-            dataFim = LocalDate.parse(formattedTextField2.getText(), frm);
-            comandasInt = cdao.buscarEmIntervalo(dataInicio, dataFim);
-        }catch(ClassNotFoundException | IOException ex){
-            JOptionPane.showMessageDialog(null, "Falha", "Mensagem de erro", JOptionPane.ERROR_MESSAGE);
-        }
+        buscarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-        Comanda[] array = new Comanda[comandasInt.size()];
-        comandasInt.toArray();
+                DateTimeFormatter frm = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        float total = 0f;
+                try{
 
-        NumberFormat nf = NumberFormat.getCurrencyInstance();
-        String[] colunas = {"Data", "Comanda", "Valor"};
-        Object[][] valores = new String[array.length][colunas.length];
-        for(int i = 0;i<array.length;i++){
-            total += array[i].calcTotal();
-            for(int k = 0; k<colunas.length; k++){
-                switch(k){
-                    case 0:
-                        valores[i][k] = array[i].getData().format(frm);
-                        break;
-                    case 1:
-                        valores[i][k] = "Comanda " +array[i].getNumeroComanda();
-                        break;
-                    case 2:
-                        valores[i][k] = nf.format(array[i].calcTotal());
-                        break;
+                    if(diniciofd.getText().equals("  /  /    ") && dfimfd.getText().equals("  /  /    ")){
+                        JOptionPane.showMessageDialog(null, "Datas vazias", "Mensagem de erro", JOptionPane.ERROR_MESSAGE);
+                        return;
+                    }else if(diniciofd.getText().equals("  /  /    ")){
+                        JOptionPane.showMessageDialog(null, "Data vazia.", "Mensagem de erro", JOptionPane.ERROR_MESSAGE);
+                        return;
+                    }else if(dfimfd.getText().equals("  /  /    ")){
+                        JOptionPane.showMessageDialog(null, "Data vazia.", "Mensagem de erro", JOptionPane.ERROR_MESSAGE);
+                        return;
+                    }
+                    dataInicio = LocalDate.parse(diniciofd.getText(), frm);
+                    dataFim = LocalDate.parse(dfimfd.getText(), frm);
+                    comandasInt = cdao.buscarEmIntervalo(dataInicio, dataFim);
+                }catch(IOException|ClassNotFoundException ex){
+                    JOptionPane.showMessageDialog(null, "Falha", "Mensagem de erro", JOptionPane.ERROR_MESSAGE);
+                }
+
+                try{
+                    if(comandasInt!=null){
+                        Comanda[] array = new Comanda[comandasInt.size()];
+                        comandasInt.toArray();
+
+                        float total = 0f;
+
+                        NumberFormat nf = NumberFormat.getCurrencyInstance();
+                        String[] colunas = {"Data", "Comanda", "Valor"};
+                        Object[][] valores = new String[array.length][colunas.length];
+                        for(int i = 0;i<array.length;i++){
+                            total += array[i].calcTotal();
+                            for(int k = 0; k<colunas.length; k++){
+                                switch(k){
+                                    case 0:
+                                        valores[i][k] = array[i].getData().format(frm);
+                                        break;
+                                    case 1:
+                                        valores[i][k] = "Comanda " +array[i].getNumeroComanda();
+                                        break;
+                                    case 2:
+                                        valores[i][k] = nf.format(array[i].calcTotal());
+                                        break;
+                                }
+                            }
+                        }
+
+                        DefaultTableModel tbm = new DefaultTableModel(valores, colunas);
+                        table1.setModel(tbm);
+                        valorTotal.setText("Total: " +nf.format(total));
+                    }
+                }catch(NullPointerException ex){
+                    JOptionPane.showMessageDialog(null, "Nenhuma comanda para mostrar", "Mensagem de erro", JOptionPane.ERROR_MESSAGE);
                 }
             }
-        }
-
-        DefaultTableModel tbm = new DefaultTableModel(valores, colunas);
-        table1.setModel(tbm);
-        valorTotal.setText("Total: " +nf.format(total));
+        });
 
     }
 
-    private void createUIComponents() {
 
-        MaskFormatter dataCmd = null;
+
+    private void createUIComponents(){
+
+        MaskFormatter dataInicioMask = null;
+        MaskFormatter dataFimMask = null;
+        diniciofd = new JFormattedTextField();
+        dfimfd = new JFormattedTextField();
+        table1 = new JTable();
 
         try{
-            dataCmd = new MaskFormatter("##/##/####");
+            dataInicioMask = new MaskFormatter("##/##/####");
+            dataFimMask = new MaskFormatter("##/##/####");
         }catch(ParseException ex){
             JOptionPane.showMessageDialog(null, ex.getMessage(), "Mensagem de erro", JOptionPane.ERROR_MESSAGE);
         }
 
-        formattedTextField1 = new JFormattedTextField();
-        formattedTextField2 = new JFormattedTextField();
-
-        if(dataCmd!=null){
-            dataCmd.install(formattedTextField1);
-            dataCmd.install(formattedTextField2);
+        if(dataInicioMask!=null && dataFimMask!=null){
+            dataInicioMask.install(diniciofd);
+            dataFimMask.install(dfimfd);
         }
 
-        Object [][] valores = {{" ", " ", " "}};
-        String colunas[] = {"Data", "Comanda", "Valor"};
-
-        DefaultTableModel tbm = new DefaultTableModel(valores, colunas);
-        table1 = new JTable();
+        String nomeCampo[] = {"Data", "Comanda", "Valor"};
+        Object [][] valores = {{"","",""}};
+        DefaultTableModel tbm = new DefaultTableModel(valores, nomeCampo);
         table1.setModel(tbm);
+        scrollPane1 = new JScrollPane(table1);
 
     }
 }
